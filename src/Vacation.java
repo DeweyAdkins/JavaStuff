7470c544006a2f58bf86f5756b01a22238432659
Dewey <kylecoder0110@gmail.com>
2024-07-22 15:02:07 -0400

    "Added Java"

---
+++ src/Vacation.java
@@ -0,0 +1,7 @@
+public class Vacation {
+        public static void main(String[] args) {
+            System.out.println("I'm planning a trip to");
+            System.out.println("Tampa");
+            System.out.println("Florida");
+        }
+}
